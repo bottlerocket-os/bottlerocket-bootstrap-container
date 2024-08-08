4c09568cd26731dd49e4a06e2f4b788c07976efa
Hieu Vu <brr2tu@virginia.edu>
2024-08-08 15:30:59 -0700

    improve bootstrap script shell style

--- bootstrap-script.sh
+++ bootstrap-script.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-set -x
+set -xeuo pipefail
 
 # Full path to the base64-encoded user data
-USER_DATA_PATH=/.bottlerocket/bootstrap-containers/current/user-data
+USER_DATA_PATH='/.bottlerocket/bootstrap-containers/current/user-data'
 
 # If the user data file is there, not empty, and not a directory, make it executable
-if [ -s "$USER_DATA_PATH" ] && [ ! -d "$USER_DATA_PATH" ]; then
-    chmod +x "$USER_DATA_PATH"
+if [[ -s "${USER_DATA_PATH}" ]] && [[ ! -d "${USER_DATA_PATH}" ]]; then
+    chmod +x "${USER_DATA_PATH}"
 
     # If the decoded script is there and executable, then execute it.
-    if [ -x "$USER_DATA_PATH" ]; then
-        echo "Executing user bootstrap script: $USER_DATA_PATH"
-        exec "$USER_DATA_PATH"
+    if [ -x "${USER_DATA_PATH}" ]; then
+        exec "${USER_DATA_PATH}"
     else
-        echo "Warning: User bootstrap script not found or not executable: $USER_DATA_PATH"
+        echo "ERROR: User bootstrap script not found or not executable: ${USER_DATA_PATH}" >&2
         exit 1
     fi
 else
-    echo "Warning: User data not found or is a directory: $USER_DATA_PATH"
+    echo "ERROR: User data not found or is a directory: ${USER_DATA_PATH}" >&2
     exit 1
 fi
